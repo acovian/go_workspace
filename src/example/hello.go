462213397032e3e7108fda489424c3c78f7c0a28
Alex Covian <alejandrocovian3@gmail.com>
2018-09-12 15:31:40 -0700

    struct

--- src/example/hello.go
+++ src/example/hello.go
@@ -4,18 +4,12 @@ import (
   "fmt"
 )
 
-func main(){
-  result, err := sqrt(16)
-  if err != nil {
-    fmt.Println(err)
-  } else {
-    fmt.Println(result)
-  }
+type person struct {
+  name string
+  age int
 }
 
-func sqrt(x float64) (float64, error) {
-  if x < 0 {
-    return 0, errors.New("Undefined for negative numbers")
-  }
-  return math.Sqrt(x), nil
+func main(){
+  p := person {name: "Jake", age:23}
+  fmt.Println(p.age)
 }
