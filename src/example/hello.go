d3b400b7298399de5dd154d9ade8c5ae44ab5111
Alex Covian <alejandrocovian3@gmail.com>
2018-09-12 15:12:25 -0700

    commit 4; for loops

--- src/example/hello.go
+++ src/example/hello.go
@@ -5,13 +5,7 @@ import (
 )
 
 func main(){
-  vertices := make(map[string]int)
-
-  vertices["triangle"] = 2
-  vertices["square"] = 3
-  vertices["dodecagon"] = 12
-
-  delete(vertices, "square")
-
-  fmt.Println(vertices)
+  for i := 0; i < 5; i++ {
+    fmt.Println(i)
+  }
 }
