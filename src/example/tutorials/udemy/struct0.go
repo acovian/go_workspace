682f042215f239d0a3b6e0440ccee2f5cfce1363
Alex Covian <alejandrocovian3@gmail.com>
2018-09-20 22:17:35 -0700

    oop example

--- src/example/tutorials/udemy/struct0.go
+++ src/example/tutorials/udemy/struct0.go
@@ -8,9 +8,13 @@ type person struct {
 	age   int
 }
 
+func (p person) fullName() string {
+	return p.first + p.last
+}
+
 func main() {
 	p1 := person{"James", "Bond", 20}
 	p2 := person{"Miss", "Moneypenny", 18}
-	fmt.Println(p1.first, p1.last, p1.age)
-	fmt.Println(p2.first, p2.last, p2.age)
+	fmt.Println(p1.fullName())
+	fmt.Println(p2.fullName())
 }
