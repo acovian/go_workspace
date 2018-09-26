b8e0a6449101095b124fa6e24c3f6458ebd43108
Alex Covian <alejandrocovian3@gmail.com>
2018-09-25 21:54:37 -0700

    completed sort.go

--- src/example/tutorials/udemy/sort.go
+++ src/example/tutorials/udemy/sort.go
@@ -5,8 +5,13 @@ import (
 	"sort"
 )
 
+type people []string
+
+func (p people) Len() int           { return len(p) }
+func (p people) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p people) Less(i, j int) bool { return p[i] < p[j] }
+
 func main() {
-	type people []string
 	studyGroup := people{"Zeno", "John", "Al", "Jenny"}
 	sort.Sort(studyGroup)
 	fmt.Println(studyGroup)
