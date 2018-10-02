165fd76c230f5f1ff307f6d876b03a58a26accb5
Alex Covian <alejandrocovian3@gmail.com>
2018-10-02 16:10:06 -0700

    added time delay to functions

--- src/example/tutorials/udemy/waitgroup.go
+++ src/example/tutorials/udemy/waitgroup.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 var wg sync.WaitGroup
@@ -17,13 +18,15 @@ func main() {
 func foo() {
 	for i := 0; i < 45; i++ {
 		fmt.Println("Foo: ", i)
+		time.Sleep(time.Duration(3 * time.Millisecond))
 	}
 	wg.Done()
 }
 
 func bar() {
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 45; i++ {
 		fmt.Println("Bar:", i)
+		time.Sleep(time.Duration(20 * time.Millisecond))
 	}
 	wg.Done()
 }
