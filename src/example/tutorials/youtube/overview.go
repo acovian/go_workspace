6023ac231296d344b4c1540b97fa52532779dfaa
Alex Covian <alejandrocovian3@gmail.com>
2018-10-26 14:01:55 -0700

    added for loops and while loops

--- src/example/tutorials/youtube/overview.go
+++ src/example/tutorials/youtube/overview.go
@@ -3,18 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	yourAge := 18
 
-	var age int = 22
-
-	var favNum float64 = 1.6180339
-
-	randNum := 1
-
-	fmt.Println(age, favNum)
-
-	var numOne = 1.000
-	var num99 = .9999
-
-	fmt.Println(numOne - num99)
+	if yourAge >= 16 {
+		fmt.Println("You Can Drive")
+	} else {
+		fmt.Println("You Can't Drive")
+	}
 }
