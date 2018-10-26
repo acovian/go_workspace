ec3f6257573401ff0473bd8d8795ac3bffd5532f
Alex Covian <alejandrocovian3@gmail.com>
2018-10-26 13:50:28 -0700

    added file for youtube tutorial

---
+++ src/example/tutorials/youtube/overview.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+
+	var age int = 22
+
+	var favNum float64 = 1.6180339
+
+	randNum := 1
+
+	fmt.Println(age, favNum)
+
+	var numOne = 1.000
+	var num99 = .9999
+
+	fmt.Println(numOne - num99)
+}
