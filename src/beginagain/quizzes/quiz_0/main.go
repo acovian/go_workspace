0e56f736eb6fb35372ba746b20880226353bdd8f
Alex Covian <alejandrocovian3@gmail.com>
2019-01-17 23:57:38 -0800

    attempted to add quiz game constructed with slices and for loop.

---
+++ src/beginagain/quizzes/quiz_0/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	questions := [3][2]string{
+		{"q: What is the most intelligent species of whale?", "cachalot"},
+		{"q: What is my favourite colour?", "yellow"},
+		{"q: What mammal is capable of powered flight?", "bats"},
+	}
+
+	for i := 0; i <= 3; i++ {
+		fmt.Println(questions[i])
+	}
+}
+
+// reader := bufio.NewReader(os.Stdin)
+// fmt.Print("What is another intelligent species on Earth?> ")
+// input, _ := reader.ReadString('\n')
+// if strings.TrimRight(strings.ToLower(input), "\n") == "cachalot" {
+// 	fmt.Println("Correct.")
+// } else {
+// 	fmt.Println("What? Try again.")
+// }
+// }
